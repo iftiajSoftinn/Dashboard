5b322e97d0f052f14118f00467fe43f991699ef2
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-03-08 22:41:57 +0800

    Id changed to NoOfRes

--- Dashboard/Controllers/DashboardController.cs
+++ Dashboard/Controllers/DashboardController.cs
@@ -16,66 +16,66 @@ namespace Dashboard.Controllers
             {
                 new ReservationModel
                 {
-                    Id = 1,
+                    NoOfRes = 1,
                     Date = new DateTime(2023, 3, 1), // // year, month, day
                     Payment = 100
                 },
 
                 new ReservationModel
                 {
-                    Id = 2,
+                    NoOfRes = 2,
                     Date = new DateTime(2023, 3, 2), // // year, month, day
                     Payment = 90
                 },                
                 new ReservationModel
                 {
-                    Id = 3,
+                    NoOfRes = 3,
                     Date = new DateTime(2023, 3, 3), // // year, month, day
                     Payment = 100
                 },
 
                 new ReservationModel
                 {
-                    Id = 4,
+                    NoOfRes = 4,
                     Date = new DateTime(2023, 3, 4), // // year, month, day
                     Payment = 90
                 },                
                 new ReservationModel
                 {
-                    Id = 5,
+                    NoOfRes = 5,
                     Date = new DateTime(2023, 3, 5), // // year, month, day
                     Payment = 100
                 },
 
                 new ReservationModel
                 {
-                    Id = 6,
+                    NoOfRes = 6,
                     Date = new DateTime(2023, 3, 6), // // year, month, day
                     Payment = 90
                 },                
                 new ReservationModel
                 {
-                    Id = 7,
+                    NoOfRes = 7,
                     Date = new DateTime(2023, 3, 7), // // year, month, day
                     Payment = 100
                 },
 
                 new ReservationModel
                 {
-                    Id = 8,
+                    NoOfRes = 8,
                     Date = new DateTime(2023, 3, 8), // // year, month, day
                     Payment = 90
                 },                
                 new ReservationModel
                 {
-                    Id = 9,
+                    NoOfRes = 9,
                     Date = new DateTime(2023, 3, 9), // // year, month, day
                     Payment = 100
                 },
 
                 new ReservationModel
                 {
-                    Id = 10,
+                    NoOfRes = 10,
                     Date = new DateTime(2023, 3, 10), // // year, month, day
                     Payment = 90
                 }
--- Dashboard/Models/ReservationModel.cs
+++ Dashboard/Models/ReservationModel.cs
@@ -2,7 +2,7 @@
 {
     public class ReservationModel
     {
-        public int Id { get; set; }
+        public int NoOfRes { get; set; }
         public DateTime Date { get; set; }
         public decimal Payment { get; set; }
     }
