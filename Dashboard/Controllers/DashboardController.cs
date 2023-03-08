713df0e35a4af166aded61d428a74e16502d9501
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-03-08 19:30:33 +0800

    Add project files.

---
+++ Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,89 @@
+﻿using Dashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReservationDetails()
+        {
+            List<ReservationModel> reservationDetails = new List<ReservationModel>()
+            {
+                new ReservationModel
+                {
+                    Id = 1,
+                    Date = new DateTime(2023, 3, 1), // // year, month, day
+                    Payment = 100
+                },
+
+                new ReservationModel
+                {
+                    Id = 2,
+                    Date = new DateTime(2023, 3, 2), // // year, month, day
+                    Payment = 90
+                },                
+                new ReservationModel
+                {
+                    Id = 3,
+                    Date = new DateTime(2023, 3, 3), // // year, month, day
+                    Payment = 100
+                },
+
+                new ReservationModel
+                {
+                    Id = 4,
+                    Date = new DateTime(2023, 3, 4), // // year, month, day
+                    Payment = 90
+                },                
+                new ReservationModel
+                {
+                    Id = 5,
+                    Date = new DateTime(2023, 3, 5), // // year, month, day
+                    Payment = 100
+                },
+
+                new ReservationModel
+                {
+                    Id = 6,
+                    Date = new DateTime(2023, 3, 6), // // year, month, day
+                    Payment = 90
+                },                
+                new ReservationModel
+                {
+                    Id = 7,
+                    Date = new DateTime(2023, 3, 7), // // year, month, day
+                    Payment = 100
+                },
+
+                new ReservationModel
+                {
+                    Id = 8,
+                    Date = new DateTime(2023, 3, 8), // // year, month, day
+                    Payment = 90
+                },                
+                new ReservationModel
+                {
+                    Id = 9,
+                    Date = new DateTime(2023, 3, 9), // // year, month, day
+                    Payment = 100
+                },
+
+                new ReservationModel
+                {
+                    Id = 10,
+                    Date = new DateTime(2023, 3, 10), // // year, month, day
+                    Payment = 90
+                }
+            };
+            ViewBag.List = reservationDetails;
+            ViewData["reservationDeatails"] = reservationDetails;
+
+            return View(reservationDetails);
+        }
+    }
+}
---
+++ Dashboard/Models/ReservationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dashboard.Models
+{
+    public class ReservationModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Payment { get; set; }
+    }
+}
