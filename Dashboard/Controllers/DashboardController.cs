c6b7b808b003722281438a8c4554bab8ef7626bd
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-03-08 23:21:06 +0800

    Past 7 days data working

--- Dashboard/Controllers/DashboardController.cs
+++ Dashboard/Controllers/DashboardController.cs
@@ -1,89 +1,92 @@
 ﻿using Dashboard.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dashboard.Controllers
 {
     public class DashboardController : Controller
     {
+        private List<ReservationModel> reservations = new List<ReservationModel>()
+        {
+            new ReservationModel
+            {
+                NoOfRes = 1,
+                Date = new DateTime(2023, 3, 1), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 2,
+                Date = new DateTime(2023, 3, 2), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 3,
+                Date = new DateTime(2023, 3, 3), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 4,
+                Date = new DateTime(2023, 3, 4), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 5,
+                Date = new DateTime(2023, 3, 5), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 6,
+                Date = new DateTime(2023, 3, 6), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 7,
+                Date = new DateTime(2023, 3, 7), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 8,
+                Date = new DateTime(2023, 3, 8), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 9,
+                Date = new DateTime(2023, 3, 9), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 10,
+                Date = new DateTime(2023, 3, 10), // year, month, day
+                Payment = 90
+            }
+        };
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ReservationDetails()
+        public ActionResult ReservationDetails(DateTime? selectedDate)
         {
-            List<ReservationModel> reservationDetails = new List<ReservationModel>()
-            {
-                new ReservationModel
-                {
-                    NoOfRes = 1,
-                    Date = new DateTime(2023, 3, 1), // // year, month, day
-                    Payment = 100
-                },
+            DateTime date = selectedDate ?? DateTime.Today;
 
-                new ReservationModel
-                {
-                    NoOfRes = 2,
-                    Date = new DateTime(2023, 3, 2), // // year, month, day
-                    Payment = 90
-                },                
-                new ReservationModel
-                {
-                    NoOfRes = 3,
-                    Date = new DateTime(2023, 3, 3), // // year, month, day
-                    Payment = 100
-                },
+            List<ReservationModel> pastReservations = reservations.Where(r => r.Date >= date.AddDays(-7) && r.Date < date).ToList();
+            List<ReservationModel> upcomingReservations = reservations.Where(r => r.Date >= date && r.Date < date.AddDays(7)).ToList();
 
-                new ReservationModel
-                {
-                    NoOfRes = 4,
-                    Date = new DateTime(2023, 3, 4), // // year, month, day
-                    Payment = 90
-                },                
-                new ReservationModel
-                {
-                    NoOfRes = 5,
-                    Date = new DateTime(2023, 3, 5), // // year, month, day
-                    Payment = 100
-                },
+            ReservationDetailsViewModel model = new ReservationDetailsViewModel(pastReservations, upcomingReservations, date);
 
-                new ReservationModel
-                {
-                    NoOfRes = 6,
-                    Date = new DateTime(2023, 3, 6), // // year, month, day
-                    Payment = 90
-                },                
-                new ReservationModel
-                {
-                    NoOfRes = 7,
-                    Date = new DateTime(2023, 3, 7), // // year, month, day
-                    Payment = 100
-                },
-
-                new ReservationModel
-                {
-                    NoOfRes = 8,
-                    Date = new DateTime(2023, 3, 8), // // year, month, day
-                    Payment = 90
-                },                
-                new ReservationModel
-                {
-                    NoOfRes = 9,
-                    Date = new DateTime(2023, 3, 9), // // year, month, day
-                    Payment = 100
-                },
-
-                new ReservationModel
-                {
-                    NoOfRes = 10,
-                    Date = new DateTime(2023, 3, 10), // // year, month, day
-                    Payment = 90
-                }
-            };
-            ViewBag.List = reservationDetails;
-            ViewData["reservationDeatails"] = reservationDetails;
-
-            return View(reservationDetails);
+            return View(model);
         }
     }
 }
---
+++ Dashboard/Models/ReservationDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Dashboard.Models
+{
+    public class ReservationDetailsViewModel
+    {
+        public ReservationDetailsViewModel(List<ReservationModel> pastReservations, List<ReservationModel> upcomingReservations, DateTime date)
+        {
+            PastReservations = pastReservations;
+            UpcomingReservations = upcomingReservations;
+            Date = date;
+        }
+
+        public IEnumerable<ReservationModel> PastReservations { get; set; }
+        public IEnumerable<ReservationModel> UpcomingReservations { get; set; }
+        public DateTime SelectedDate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
