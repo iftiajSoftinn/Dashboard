985e90b3f7fa55c0eeb5929911a8c446eb558ee6
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-03-09 00:43:19 +0800

    -Display past and uncomming 7 days data done -Count total number of (Past and upcoming 7 days) done -Sum total payment of (Past and upcoming 7 days) done

--- Dashboard/Controllers/DashboardController.cs
+++ Dashboard/Controllers/DashboardController.cs
@@ -69,6 +69,126 @@ namespace Dashboard.Controllers
                 NoOfRes = 10,
                 Date = new DateTime(2023, 3, 10), // year, month, day
                 Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 11,
+                Date = new DateTime(2023, 3, 11), // year, month, day
+                Payment = 150
+            },
+            new ReservationModel
+            {
+                NoOfRes = 12,
+                Date = new DateTime(2023, 3, 12), // year, month, day
+                Payment = 80
+            },
+            new ReservationModel
+            {
+                NoOfRes = 13,
+                Date = new DateTime(2023, 3, 13), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 14,
+                Date = new DateTime(2023, 3, 14), // year, month, day
+                Payment = 110
+            },
+            new ReservationModel
+            {
+                NoOfRes = 15,
+                Date = new DateTime(2023, 3, 15), // year, month, day
+                Payment = 150
+            },
+            new ReservationModel
+            {
+                NoOfRes = 16,
+                Date = new DateTime(2023, 3, 16), // year, month, day
+                Payment = 140
+            },
+            new ReservationModel
+            {
+                NoOfRes = 17,
+                Date = new DateTime(2023, 3, 17), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 18,
+                Date = new DateTime(2023, 3, 18), // year, month, day
+                Payment = 150
+            },
+            new ReservationModel
+            {
+                NoOfRes = 19,
+                Date = new DateTime(2023, 3, 19), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 20,
+                Date = new DateTime(2023, 3, 20), // year, month, day
+                Payment = 90
+            },
+                        new ReservationModel
+            {
+                NoOfRes = 21,
+                Date = new DateTime(2023, 3, 21), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 22,
+                Date = new DateTime(2023, 3, 22), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 3,
+                Date = new DateTime(2023, 3, 23), // year, month, day
+                Payment = 140
+            },
+            new ReservationModel
+            {
+                NoOfRes = 4,
+                Date = new DateTime(2023, 3, 24), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 5,
+                Date = new DateTime(2023, 3, 25), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 6,
+                Date = new DateTime(2023, 3, 26), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 7,
+                Date = new DateTime(2023, 3, 27), // year, month, day
+                Payment = 120
+            },
+            new ReservationModel
+            {
+                NoOfRes = 8,
+                Date = new DateTime(2023, 3, 28), // year, month, day
+                Payment = 90
+            },
+            new ReservationModel
+            {
+                NoOfRes = 9,
+                Date = new DateTime(2023, 3, 29), // year, month, day
+                Payment = 100
+            },
+            new ReservationModel
+            {
+                NoOfRes = 10,
+                Date = new DateTime(2023, 3, 30), // year, month, day
+                Payment = 90
             }
         };
 
@@ -83,6 +203,11 @@ namespace Dashboard.Controllers
 
             List<ReservationModel> pastReservations = reservations.Where(r => r.Date >= date.AddDays(-7) && r.Date < date).ToList();
             List<ReservationModel> upcomingReservations = reservations.Where(r => r.Date >= date && r.Date < date.AddDays(7)).ToList();
+
+            int totalPastNoOfRes = pastReservations.Sum(r => r.NoOfRes);
+            decimal totalPastPayment = pastReservations.Sum(r => r.Payment);
+            int totalUpcomingNoOfRes = upcomingReservations.Sum(r => r.NoOfRes);
+            decimal totalUpcomingPayment = upcomingReservations.Sum(r => r.Payment);
 
             ReservationDetailsViewModel model = new ReservationDetailsViewModel(pastReservations, upcomingReservations, date);
 
--- Dashboard/Models/ReservationDetailsViewModel.cs
+++ Dashboard/Models/ReservationDetailsViewModel.cs
@@ -1,17 +1,29 @@
 ﻿namespace Dashboard.Models
 {
     public class ReservationDetailsViewModel
+
+
     {
         public ReservationDetailsViewModel(List<ReservationModel> pastReservations, List<ReservationModel> upcomingReservations, DateTime date)
         {
             PastReservations = pastReservations;
             UpcomingReservations = upcomingReservations;
             Date = date;
+
+            TotalPastNoOfRes = pastReservations.Sum(r => r.NoOfRes);
+            TotalPastPayment = pastReservations.Sum(r => r.Payment);
+
+            TotalUpcomingNoOfRes = upcomingReservations.Sum(r => r.NoOfRes);
+            TotalUpcomingPayment = upcomingReservations.Sum(r => r.Payment);
         }
 
-        public IEnumerable<ReservationModel> PastReservations { get; set; }
-        public IEnumerable<ReservationModel> UpcomingReservations { get; set; }
-        public DateTime SelectedDate { get; set; }
         public DateTime Date { get; set; }
+        public int TotalPastNoOfRes { get; set; }
+        public decimal TotalPastPayment { get; set; }
+        public List<ReservationModel> PastReservations { get; set; }
+        public int TotalUpcomingNoOfRes { get; set; }
+        public decimal TotalUpcomingPayment { get; set; }
+        public List<ReservationModel> UpcomingReservations { get; set; }
     }
+
 }
